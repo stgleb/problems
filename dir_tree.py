9cf41087d244e6b9fafa3b8b39774f6ba3340434
Gleb Stepanov <gstepanov@mirantis.com>
2016-11-10 18:30:48 +0200

    Recursively traverse directory

---
+++ dir_tree.py
@@ -0,0 +1,19 @@
+import os
+import stat
+
+
+def walktree(root):
+    for file in os.listdir(root):
+        path = os.path.join(root, file)
+        mode = os.stat(path).st_mode
+
+        if stat.S_ISDIR(mode):
+            walktree(path)
+        elif stat.S_ISREG(mode):
+            print(file)
+        else:
+            print("unkown file type")
+
+
+if __name__ == "__main__":
+    walktree(os.getcwd())
