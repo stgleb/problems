881f16ad2e4cf34307d28e628bf9af36be3004fc
stgleb <glebstepanov1992@gmail.com>
2016-11-20 00:29:15 +0200

    Find pairs of element with sum K

---
+++ python/array/find_pair_sum.py
@@ -0,0 +1,24 @@
+"""
+Given an array pf numbers, find whether there is a pair
+of numbers in the array whose sum is equal to K. Time complexity
+O(N).
+"""
+
+
+def solution(A, K):
+    d = dict()
+    
+    for v in A:
+        d[v] = True
+        tmp = K - v
+
+        if tmp in d:
+            return True
+
+    return False
+
+
+if __name__ == "__main__":
+    A = [4, 1, 3, 8, 2, 8, 6, 7, 5]
+    K = 11
+    print(solution(A, K))
