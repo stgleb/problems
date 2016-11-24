9d2367b105a22ed6f12e1228cace77a44faeeda6
Gleb Stepanov <gstepanov@mirantis.com>
2016-11-24 12:24:34 +0200

    Add iterative solution for word break problem

--- python/backtracking/words_break.py
+++ python/backtracking/words_break.py
@@ -9,6 +9,14 @@ dict = ["sample", "test", "word", "string", "test"]
 
 
 def word_break(s, d, breaks, cur=""):
+    """
+    Recursive solution
+    :param s: original text
+    :param d: dictionary
+    :param breaks: set to handler the result
+    :param cur: current split of text
+    :return:
+    """
     if not s:
         breaks.add(cur)
         return
@@ -18,9 +26,35 @@ def word_break(s, d, breaks, cur=""):
             word_break(s[i:], d, breaks, cur + " " + s[:i])
 
 
+def word_break_iteratively(s, d):
+    """
+    Iterative solution using stack
+
+    :param s: original text
+    :param d: dictionary of words
+    :return:
+    """
+    stack = [[s, []]]
+    result = []
+
+    while stack:
+        text, breaks = stack.pop()
+
+        if not text:
+            result.append(breaks)
+            continue
+
+        for i in range(len(text) + 1):
+            if text[:i] in d:
+                stack.append([text[i:], breaks + [text[:i]]])
+
+    return result
+
+
 if __name__ == "__main__":
     s = "sampleteststring"
     d = {"sample", "test", "word", "string", "test", "te", "st"}
     result = set()
     word_break(s, d, result)
     print(result)
+    print(word_break_iteratively(s, d))
