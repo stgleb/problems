e9fa95a175b588002221ccccc4fbe702a9e060bb
stgleb <glebstepanov1992@gmail.com>
2016-11-26 16:58:12 +0200

    Swap three numbers

---
+++ python/bit_magic/swap_three_variables.py
@@ -0,0 +1,21 @@
+def swap_arithm(a, b, c):
+    a = a + b + c
+    b = a - b - c
+    c = a - b - c
+    a = a - b - c
+
+    return a, b, c
+
+
+def swap_bin(a, b, c):
+    a = a ^ b ^ c
+    b = a ^ b ^ c
+    c = a ^ b ^ c
+    a = a ^ b ^ c
+
+    return a, b, c
+
+
+if __name__ == "__main__":
+    print(swap_arithm(1, 2, 3))
+    print(swap_bin(1, 2, 3))
