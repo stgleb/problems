32790a5eec11d1322b4c9aed093647bcd84b2725
stgleb <glebstepanov1992@gmail.com>
2016-11-27 13:55:30 +0200

    Add naive count bit solution Add comments for tricky algorithm

--- python/bit_magic/count_total_bit_set.py
+++ python/bit_magic/count_total_bit_set.py
@@ -24,6 +24,14 @@ def next_leftmost_bit_set(n, m):
 
 
 def count_bit_util(n, m):
+    """
+    If number n is power of 2  - 1 - return answer,
+    otherwise count number of set bits when m bit is 1,
+    and call recursive for count bits when m bit is 0.
+    :param n:
+    :param m:
+    :return:
+    """
     if n == 0:
         return 0
 
@@ -38,9 +46,40 @@ def count_bit_util(n, m):
 
 
 def count_bit(n):
+    """
+    Solution with O(LogN) time complexity, based on rule that
+    count if set bits for [2^n, 2^(n - 1)) is n * 2 ^(n - 1).
+    :param n:
+    :return:
+    """
     m = leftmost_bit_set(n)
     return count_bit_util(n, m)
 
 
+def count_bit_set_naive_util(n):
+    cnt = 0
+
+    while n:
+        cnt += n & 1
+        n >>= 1
+
+    return cnt
+
+
+def count_bit_set_naive(n):
+    """
+    Naive O(N) solution
+    :param n:
+    :return:
+    """
+    cnt = 0
+
+    for i in range(1, n + 1):
+        cnt += count_bit_set_naive_util(i)
+
+    return cnt
+
+
 if __name__ == "__main__":
     print(count_bit(7))
+    print(count_bit_set_naive(7))
