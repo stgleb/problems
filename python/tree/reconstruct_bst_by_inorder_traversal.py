5dfe3c83c6614384002eb91cfb2844eb2fef0ce4
Gleb Stepanov <gstepanov@mirantis.com>
2016-12-02 13:03:36 +0200

    Reconstruct bst by inorder traversal

--- python/tree/reconstruct_bst_by_inorder_traversal.py
+++ python/tree/reconstruct_bst_by_inorder_traversal.py
@@ -6,9 +6,18 @@ Given in-order traversal of binary search tree, reconstruct original tree.
 from python.tree.node import Node
 
 
-
 def reconstruct(arr):
-    pass
+    if not arr:
+        return
+
+    mid = len(arr) >> 1
+    root_value = arr[mid]
+    root = Node(root_value)
+
+    root.left = reconstruct(arr[:mid])
+    root.right = reconstruct(arr[mid + 1:])
+
+    return root
 
 
 def inorder_traversal(root):
@@ -21,6 +30,6 @@ def inorder_traversal(root):
 
 
 if __name__ == "__main__":
-    arr = [10, 5, 1, 7, 40, 50]
+    arr = [1, 5, 7, 10, 40, 50]
     root = reconstruct(arr)
     inorder_traversal(root)
