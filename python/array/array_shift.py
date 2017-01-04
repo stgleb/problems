79d1d8126f0170f15bc771c444605e1c495076c6
Gleb Stepanov <gstepanov@mirantis.com>
2017-01-04 17:46:38 +0200

    Array shift simple

---
+++ python/array/array_shift.py
@@ -0,0 +1,44 @@
+"""
+A zero-indexed array A consisting of N integers is given. Rotation of the
+array means that each element is shifted right by one index, and the last element
+of the array is also moved to the first place.
+
+For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7].
+The goal is to rotate array A K times; that is, each element of A will be
+shifted to the right by K indexes.
+
+Write a function:
+
+object Solution { def solution(a: Array[Int], k: Int): Array[Int] }
+
+that, given a zero-indexed array A consisting of N integers and an integer K,
+returns the array A rotated K times.
+
+For example, given array A = [3, 8, 9, 7, 6] and K = 3, the function should return [9, 7, 6, 3, 8].
+"""
+
+
+def _shift(A):
+    tmp = A[len(A) - 1]
+
+    for i in range(len(A) - 2, -1, -1):
+        A[i + 1] = A[i]
+
+    A[0] = tmp
+
+
+def shift_array(A, K):
+    """
+    The simplest solution
+    :param A:
+    :param K:
+    :return:
+    """
+    for i in range(K):
+        _shift(A)
+
+
+if __name__ == "__main__":
+    A = [3, 8, 9, 7, 6]
+    shift_array(A, 3)
+    print(A)
