881cb3fdc01d1691ed72b665f9c2468064b3f7c6
Gleb Stepanov <gstepanov@mirantis.com>
2017-01-11 12:36:50 +0200

    Fizz Buzz problem

---
+++ python/array/fizz_buzz.py
@@ -0,0 +1,21 @@
+"""
+Write a program that prints the numbers from 1 to 100. But for multiples of three print
+Fizz instead of the number and for the multiples of five print Buzz. For numbers which
+ are multiples of both three and five print FizzBuzz.
+"""
+
+
+def fizz_buzz(n):
+    for i in range(n):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+if __name__ == "__main__":
+    fizz_buzz(100)
